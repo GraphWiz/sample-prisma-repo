503cce76c3b0fe6fc4f65bf8d1f9580e34f8f1ae
Jaypee <jignacio@cambridge.org>
2023-06-30 18:27:08 +0800

    Fix content for automation

--- generate.sh
+++ generate.sh
@@ -1,24 +1,46 @@
 #!/bin/bash
 
-# Find Prisma schema file
+# Check if the required arguments are provided
+if [[ $# -ne 2 ]]; then
+  echo "Usage: ./generate.sh <endpoint> <x-api-header>"
+  exit 1
+fi
+
 prisma_file=$(find . -name '*.prisma' -print -quit)
 
-# Check if Prisma schema file exists
 if [[ -z $prisma_file ]]; then
   echo "Prisma schema file not found in the repository."
   exit 1
 fi
 
-# Create a backup of the original Prisma schema file
 backup_file="${prisma_file}.bak"
 cp "$prisma_file" "$backup_file"
 
-# Remove generator and datasource blocks from the Prisma schema file
 sed -i '/^generator\|datasource/,/^\}/d' "$backup_file"
 
-# Read the modified content of the Prisma schema file
-prisma_content=$(cat "$backup_file")
+start_line=$(grep -m 1 -n '\S' "$backup_file" | cut -d: -f1)
+end_line=$(grep -n '\S' "$backup_file" | tail -n 1 | cut -d: -f1)
 
-# Print the modified content
+sed -i "1,$((start_line-1))d; $((end_line+1)),$ d" "$backup_file"
+
 echo "Prisma schema file content (after removing generator and datasource blocks):"
-echo "$prisma_content"
+cat "$backup_file"
+
+
+# Create the JSON payload
+json_payload=$(jq -n --arg message "$(cat "$backup_file")" '{ "type": "erd", "message": $message, "model": "gpt-4" }')
+
+# Make the POST request
+response=$(curl -X POST \
+  -H "Content-Type: application/json" \
+  -H "x-api-header: $2" \
+  -d "$json_payload" \
+  "$1")
+
+# Extract the response text
+response_text=$(echo "$response" | jq -r '.response.text')
+
+# Save the response text to a markdown file
+echo "$response_text" > erd-output.md
+
+rm -f "$backup_file"
